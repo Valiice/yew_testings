9fa5437bfc12cd3a6dbb1cf131658a3e3c057519
Valiice <valentinclaes@live.be>
2023-04-22 13:04:13 +0200

    add rust yew start

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+use gloo::console;
+use yew::prelude::*;
+
+enum Msg {
+    Increment,
+    Decrement,
+}
+
+struct Model {
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { value: 0 }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Increment => {
+                self.value += 1;
+                console::log!("plus one"); // Will output a string to the browser console
+                true // Return true to cause the displayed change to update
+            }
+            Msg::Decrement => {
+                self.value -= 1;
+                console::log!("minus one");
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <div>
+                <div class="panel">
+                    // A button to send the Increment message
+                    <button class="button" onclick={ctx.link().callback(|_| Msg::Increment)}>
+                        { "+1" }
+                    </button>
+
+                    // A button to send the Decrement message
+                    <button onclick={ctx.link().callback(|_| Msg::Decrement)}>
+                        { "-1" }
+                    </button>
+
+                    // A button to send two Increment messages
+                    <button onclick={ctx.link().batch_callback(|_| vec![Msg::Increment, Msg::Increment])}>
+                        { "+1, +1" }
+                    </button>
+
+                </div>
+
+                // Display the current value of the counter
+                <p class="counter">
+                    { self.value }
+                </p>
+            </div>
+        }
+    }
+}
+
+fn main() {
+    yew::Renderer::<Model>::new().render();
+}
